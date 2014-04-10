57799608ca5359ad0b5fcee054b52bd9b251fee7
Héctor García <hector@debian.org>
2014-04-10 13:27:25 +0200

    allmost done gettext support

--- src/mssh.c
+++ src/mssh.c
@@ -246,9 +246,9 @@ int main(int argc, char* argv[], char* env[])
         {0, 0, 0, 0}
     };
 
-    bindtextdomain(GETTEXT_PACKAGE, PROGRAMNAME_LOCALEDIR);
-    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
-    textdomain(GETTEXT_PACKAGE);
+    bindtextdomain(PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset(PACKAGE, "UTF-8");
+    textdomain(PACKAGE);
 
     if((home = getenv("HOME")) != NULL)
     {
